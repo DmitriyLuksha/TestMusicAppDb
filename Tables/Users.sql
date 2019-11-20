481ac7c8f6bef6a64e89efc4595feae1e85f4cce
DmitriyLuksha <nanpxmail@gmail.com>
2019-11-20 22:59:00 +0300

    Set up users database

--- Tables/Users.sql
+++ Tables/Users.sql
@@ -1,6 +1,15 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[UserId] INT NOT NULL PRIMARY KEY, 
-    [Username] NVARCHAR(30) NULL,
+	[ClusteredId] INT NOT NULL IDENTITY, 
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED, 
+    [Email] NVARCHAR(256) NULL, 
+    [Username] NVARCHAR(50) NULL, 
+    [Password] VARCHAR(40) NULL, 
+    CONSTRAINT [UN_Users_Username] UNIQUE(Username),
+	CONSTRAINT [UN_Users_Email] UNIQUE(Email)
 
 )
+
+GO
+
+CREATE CLUSTERED INDEX [IX_Users_ClusteredId] ON [dbo].[Users] ([ClusteredId])
