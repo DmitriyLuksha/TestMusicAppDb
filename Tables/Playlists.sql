66a01cf6b4781141e9e0cdd7b0820fe01c80ad55
DmitriyLuksha <nanpxmail@gmail.com>
2019-12-15 13:18:51 +0300

    Add playlists table

---
+++ Tables/Playlists.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Playlists]
+(
+	[ClusteredId] INT NOT NULL IDENTITY, 
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    CONSTRAINT [FK_Playlists_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]), 
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_Playlists_ClusteredId] ON [dbo].[Playlists] ([ClusteredId])
