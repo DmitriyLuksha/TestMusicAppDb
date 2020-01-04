38afd763714d4f37ff26f5fbe7d83a1e553c1bff
DmitriyLuksha <nanpxmail@gmail.com>
2020-01-04 13:45:38 +0300

    Add tracks table

---
+++ Tables/Tracks.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Tracks]
+(
+	[ClusteredId] INT NOT NULL IDENTITY, 
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED, 
+	[PlaylistId] UNIQUEIDENTIFIER NOT NULL, 
+    [TrackName] NVARCHAR(100) NOT NULL, 
+    [FileName] VARCHAR(200) NOT NULL, 
+    CONSTRAINT [FK_Tracks_PlaylistId] FOREIGN KEY ([PlaylistId]) REFERENCES [Playlists]([Id]), 
+)
+
+GO
+
+CREATE CLUSTERED INDEX [IX_Tracks_ClusteredId] ON [dbo].[Tracks] ([ClusteredId])
